7c5f8a704122d47a3194911cb611967b3c42b074
並木裕介 <namikiyuusuke@namikiyuusukenoMacBook-Pro.local>
2024-10-03 22:06:27 +0900

    24-Oct3rd-5th

--- routes/web.php
+++ routes/web.php
@@ -29,24 +29,18 @@ Route::get('/list', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::group(['middleware' => 'auth'], function () {
     
-    // 商品一覧
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
-    // 商品登録フォーム表示
     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-    
-    // 商品登録処理
+
     Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 
-    // 商品詳細表示
     Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 
-    // 商品編集フォーム表示
     Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
     
-    // 商品更新処理
     Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
 
-    // 商品削除処理
     Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    
 });
