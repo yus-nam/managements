87bd60c0d75a7e06484187b4ecaf396092c19ba9
並木裕介 <namikiyuusuke@namikiyuusukenoMacBook-Pro.local>
2024-10-27 17:28:30 +0900

    24-Oct27th-2nd

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Sale;
@@ -32,6 +33,8 @@ class SalesController extends Controller
 
         $sale = new Sale([
             'product_id' => $productId,
+            'quantity' => $quantity,
+            'price' => $product->price * $quantity,
         ]);
 
         $sale->save();
